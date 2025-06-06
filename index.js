b4ea35e3e12d1019eb68bd2859ae16f43e8da4de
olerk16 <netlocolerk16@hotmail.com>
2025-06-06 08:45:04 -0500

    spa 6.3

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 import { header, nav, main, footer } from "./components";
+import * as store from "./store";
+import Navigo from "navigo";
+import { camelCase } from "lodash";
 
-function render() {
+const router = new Navigo("/");
+
+function render(state = store.home) {
   document.querySelector("#root").innerHTML = `
-      ${header()}
-      ${nav()}
-      ${main("aboutMe")}
+      ${header(state)}
+      ${nav(store.links)}
+      ${main(state)}
       ${footer()}
     `;
+
+    router.updatePageLinks();
 }
 
-render();
+router.on("/", () => render()).resolve();
+
+router.on({
+  "/": () => render(),
+  // The :view slot will match any single URL segment that appears directly after the domain name and a slash
+  '/:view': function(match) {
+    // If URL is '/about-me':
+    // match.data.view will be 'about-me'
+    // Using Lodash's camelCase to convert kebab-case to camelCase:
+    // 'about-me' becomes 'aboutMe'
+    const view = match?.data?.view ? camelCase(match.data.view) : "home";
+
+    // If the store import/object has a key named after the view
+    if (view in store) {
+      // Then the invoke the render function using the view state, using the view name
+      render(store[view]);
+    } else {
+      // If the store
+      render(store.viewNotFound);
+      console.log(`View ${view} not defined`);
+    }
+  }
+});
+
 
 // add menu toggle to bars icon in nav bar
 document.querySelector(".fa-bars").addEventListener("click", () => {
---
+++ store/aboutMe.js
@@ -0,0 +1,4 @@
+ export default {
+   header: "this is about me",
+   view: "aboutMe"
+ };
