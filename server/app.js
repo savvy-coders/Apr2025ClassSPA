1432d17d7cd840f89f1e675a84fdc469e54c9ac6
Matt Thomas <matsinet@gmail.com>
2025-06-18 21:32:44 -0500

    lesson 8.3 complete, lesson 9.1 started

--- server/app.js
+++ server/app.js
@@ -1,7 +1,36 @@
 // "Import" the Express module instead of http
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import pizzas from "./controllers/pizzas.js"
+
+// Load environment variables from .env file
+dotenv.config();
+
+mongoose.connect( process.env.MONGODB );
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+// get the PORT from the environment variables, OR use 3000 as default
+const PORT = process.env.PORT || 3000;
+
 // Initialize the Express application
 const app = express();
+
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${new Date().toLocaleString("en-us")}`);
+  next();
+};
+
+app.use(cors());
+app.use(express.json());
+app.use(logging);
 
 // Handle the request with HTTP GET method from http://localhost:3000/
 app.get("/", (request, response) => {
@@ -16,6 +45,37 @@ app.get("/status", (request, response) => {
   response.json({ message: "Service healthy" });
 });
 
+// Handle the request with HTTP GET method with query parameters and a url parameter
+app.get("/weather/:city", (request, response) => {
+  // Express adds a "params" Object to requests that has an matches parameter created using the colon syntax
+  const city = request.params.city;
+
+  // Set defaults values for the query string parameters
+  let lowTemp = 32;
+  if ("lowtemp" in request.query) {
+    lowTemp = Number(request.query.lowtemp);
+  }
+
+  // Generate a random number to use as the temperature
+  // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+  const min = 70;
+  const max = 90;
+  const highTemp = Math.floor(Math.random() * (max - min + 1) + min);
+
+  // handle GET request for weather with an route parameter of "city"
+  response.json({
+    text: `The weather in ${city} is ${highTemp} degrees today.`,
+    temp: {
+      current: highTemp,
+      low: lowTemp
+    },
+    city
+  });
+});
+
+// Handles any URL that starts with "/pizzas", Example: http://localhost:3000/pizzas
+app.use("/pizzas", pizzas);
+
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 3000
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ server/controllers/pizzas.js
@@ -0,0 +1,10 @@
+import { Router } from  "express";
+import Pizza from "../models/Pizza.js";
+
+const router = Router();
+
+
+
+
+
+export default router;
